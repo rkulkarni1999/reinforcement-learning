782a0a4654d8b6822861625160cdd47291896236
rkulkarni1999 <rkulkarni1@wpi.edu>
2024-10-08 20:21:35 -0400

    check added to see if project1 and project2 are working

---
+++ check_projects.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stops script if any script fails
+set -e  
+
+# Project 1
+nosetests -v Project1/mdp_dp_test.py
+
+# Project 2 - Part 1
+nosetests -v Project2/Project2-1/mc_test.py
+
+# Project 2 - Part 2 
+nosetests -v Project2/Project2-2/td_test.py
